f159f9c466dc0e898d7a662fc87ab5dbd79b834a
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2022-09-26 21:19:58 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react';
 function App() {
+  const [value, setValue]=useState(1);
+  const [count, setCount]=useState(0);
+  
+  useEffect(() => {
+    
+    setCount(count+1)
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Manokamna Singh
-        </a>
-      </header>
+      <h1 style={{backgroundColor:"lightblue"}}>Hello</h1>
+      
+      <h1 style={{color: "red"}} >hello</h1>
+      <h4>Updated{count} times...</h4>
+      <button onClick={()=>setValue(value+1)}>Take the shot!</button>
     </div>
   );
 }
